46751c078c83359809c3cf0da4b7896d8c2f6fcb
ketan chavan <chavanketan79@gmail.com>
2022-10-16 20:49:05 +0530

    database connected

--- app.js
+++ app.js
@@ -13,24 +13,24 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 //Routes
-const categoriesRoutes = require("./routes/categories");
-const productsRoutes = require("./routes/products");
-const usersRoutes = require("./routes/users");
-const ordersRoutes = require("./routes/orders");
+// const categoriesRoutes = require("./routes/categories");
+// const productsRoutes = require("./routes/products");
+// const usersRoutes = require("./routes/users");
+// const ordersRoutes = require("./routes/orders");
 
 const api = process.env.API_URL;
 
-app.use(`${api}/categories`, categoriesRoutes);
-app.use(`${api}/products`, productsRoutes);
-app.use(`${api}/users`, usersRoutes);
-app.use(`${api}/orders`, ordersRoutes);
+// app.use(`${api}/categories`, categoriesRoutes);
+// app.use(`${api}/products`, productsRoutes);
+// app.use(`${api}/users`, usersRoutes);
+// app.use(`${api}/orders`, ordersRoutes);
 
 //Database
 mongoose
   .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: "eshop-database",
+    dbName: "City_project_DB",
   })
   .then(() => {
     console.log("Database Connection is ready...");
