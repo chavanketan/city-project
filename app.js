f4ed6220b7e187939d2f004e4fad0b2090f2e3a1
ketan chavan <chavanketan79@gmail.com>
2022-10-16 21:31:52 +0530

    change routes

--- app.js
+++ app.js
@@ -12,18 +12,10 @@ app.options("*", cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
-//Routes
-// const categoriesRoutes = require("./routes/categories");
-// const productsRoutes = require("./routes/products");
-// const usersRoutes = require("./routes/users");
-// const ordersRoutes = require("./routes/orders");
+
 
 const api = process.env.API_URL;
 
-// app.use(`${api}/categories`, categoriesRoutes);
-// app.use(`${api}/products`, productsRoutes);
-// app.use(`${api}/users`, usersRoutes);
-// app.use(`${api}/orders`, ordersRoutes);
 
 //Database
 mongoose
