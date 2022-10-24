1e9c6baaf06ace5ea21f1497171ef6ed28c1d0b2
ketan chavan <chavanketan79@gmail.com>
2022-10-24 17:26:32 +0530

    ignored node modules

--- routers/busRoute.js
+++ routers/busRoute.js
@@ -1,9 +1,9 @@
-const {Bus} = require('../models/Bus.js');
+const {bus} = require('../models/Bus.js');
 const express = require('express');
 const router = express.Router();
 
 router.get(`/`, async (req, res) =>{
-    const busList = await Bus.find();
+    const busList = await bus.find();
 
     if(!busList) {
         res.status(500).json({success: false})
@@ -12,18 +12,28 @@ router.get(`/`, async (req, res) =>{
 })
 
 router.post(`/`, (req, res) =>{
-    const product = new bus({
+    const buses = new bus({
         bus_name: req.body.bus_name,
         bus_root: req.body.bus_root,
     })
 
-    product.save().then((newBus=> {
+    buses.save().then((newBus=> {
         res.status(201).json(newBus)
     })).catch((err)=>{
         res.status(500).json({
             error: err,
             success: false
         })
+    })
+})
+router.delete('/:id',(req,res)=>{
+    const busremove=bus.findIdAndRemove(req.params.id).then(buss=>{
+        if(buss){
+            return res.status(200).json({success: true,message:"bus removed from database "+buss})
+        }
+        else{
+            return res.status(2404).json({success: false,message:"no bus data found "+buss})
+        }
     })
 })
 
