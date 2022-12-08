ea1342ff10b4404f84eba9f9996c80280899d337
chavanketan <67420970+chavanketan@users.noreply.github.com>
2022-12-08 19:22:51 +0530

    Update register.js

--- models/register.js
+++ models/register.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const registerSchema = mongoose.Schema({
     username: String,
+    password:String,
+    email: String,
+    longitude: double,
+    latitude:double,
+    IsActive: Boolean,
+    business: object,
     mobile_number:Number,
     gender:String,
     password: String,
